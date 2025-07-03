fea41c43fed25046a54a36f5ab84013f239223d6
Artem Makarkin <a.makarkin@innopolis.university>
2025-07-03 06:34:44 +0300

    src/main.tsx remove duplicate themeprovider and cssbaseline

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import theme from './theme/theme';
 import './index.css'
 import App from './App.tsx'
-import {CssBaseline, ThemeProvider} from "@mui/material";
 import {HashRouter} from "react-router-dom";
 
 const root = createRoot(
@@ -11,11 +9,8 @@ const root = createRoot(
 );
 root.render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline/> {/* Сбрасываем стандартные стили браузера */}
-            <HashRouter>
-                <App/>
-            </HashRouter>
-        </ThemeProvider>
+        <HashRouter>
+            <App/>
+        </HashRouter>
     </StrictMode>
 );
